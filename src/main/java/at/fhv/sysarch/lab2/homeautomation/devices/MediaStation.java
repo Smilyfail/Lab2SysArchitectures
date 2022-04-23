ea82739484cea9afe813afade493757f4098411c
unknown <dario.birbamer@gmail.com>
2022-04-23 11:00:09 +0200

    Implemented media station, notifies blinds of its status, added command option to ui, spawner added.

--- src/main/java/at/fhv/sysarch/lab2/homeautomation/devices/MediaStation.java
+++ src/main/java/at/fhv/sysarch/lab2/homeautomation/devices/MediaStation.java
@@ -1,4 +1,58 @@
 package at.fhv.sysarch.lab2.homeautomation.devices;
 
-public class MediaStation {
+import akka.actor.typed.ActorRef;
+import akka.actor.typed.Behavior;
+import akka.actor.typed.PostStop;
+import akka.actor.typed.javadsl.AbstractBehavior;
+import akka.actor.typed.javadsl.ActorContext;
+import akka.actor.typed.javadsl.Behaviors;
+import akka.actor.typed.javadsl.Receive;
+
+import java.util.Optional;
+
+public class MediaStation extends AbstractBehavior<MediaStation.MovieCommand> {
+
+    public interface MovieCommand {}
+
+    public final static class ReadMediaStationStatus implements MovieCommand {
+        final Optional<Boolean> isRunning;
+
+        public ReadMediaStationStatus(Optional<Boolean> isRunning) {
+            this.isRunning = isRunning;
+        }
+    }
+
+    private final String groupId;
+    private final String deviceId;
+    private ActorRef<Blinds.BlindsCommand> blinds;
+
+    public static Behavior<MovieCommand> create(ActorRef<Blinds.BlindsCommand> blinds, String groupId, String deviceId){
+        return Behaviors.setup(context -> new MediaStation(context, blinds, groupId, deviceId));
+    }
+
+    public MediaStation(ActorContext<MovieCommand> context, ActorRef<Blinds.BlindsCommand> blinds, String groupId, String deviceId) {
+        super(context);
+        this.blinds = blinds;
+        this.groupId = groupId;
+        this.deviceId = deviceId;
+    }
+
+    @Override
+    public Receive<MovieCommand> createReceive() {
+        return newReceiveBuilder()
+                .onMessage(ReadMediaStationStatus.class, this::onReadMediaStationStatus)
+                .onSignal(PostStop.class, signal -> onPostStop())
+                .build();
+    }
+
+    private Behavior<MovieCommand> onReadMediaStationStatus(ReadMediaStationStatus mediaStationStatus) {
+        getContext().getLog().info("MediaStation received {}", mediaStationStatus.isRunning.get());
+        this.blinds.tell(new Blinds.MediaStationPower(mediaStationStatus.isRunning));
+        return this;
+    }
+
+    private MediaStation onPostStop() {
+        getContext().getLog().info("MediaStation actor {}-{} stopped", groupId, deviceId);
+        return this;
+    }
 }
--- src/main/java/at/fhv/sysarch/lab2/homeautomation/ui/UI.java
+++ src/main/java/at/fhv/sysarch/lab2/homeautomation/ui/UI.java
@@ -8,6 +8,7 @@ import akka.actor.typed.javadsl.ActorContext;
 import akka.actor.typed.javadsl.Behaviors;
 import akka.actor.typed.javadsl.Receive;
 import at.fhv.sysarch.lab2.homeautomation.devices.AirCondition;
+import at.fhv.sysarch.lab2.homeautomation.devices.MediaStation;
 import at.fhv.sysarch.lab2.homeautomation.devices.sensors.TemperatureSensor;
 import at.fhv.sysarch.lab2.homeautomation.devices.sensors.WeatherSensor;
 
@@ -19,17 +20,20 @@ public class UI extends AbstractBehavior<Void> {
     private ActorRef<TemperatureSensor.TemperatureCommand> tempSensor;
     private ActorRef<AirCondition.AirConditionCommand> airCondition;
     private ActorRef<WeatherSensor.WeatherCommand> weatherSensor;
+    ActorRef<MediaStation.MovieCommand> mediaStation;
 
     public static Behavior<Void> create(ActorRef<TemperatureSensor.TemperatureCommand> tempSensor,
                                         ActorRef<AirCondition.AirConditionCommand> airCondition,
-                                        ActorRef<WeatherSensor.WeatherCommand> weatherSensor) {
-        return Behaviors.setup(context -> new UI(context, tempSensor, airCondition, weatherSensor));
+                                        ActorRef<WeatherSensor.WeatherCommand> weatherSensor,
+                                        ActorRef<MediaStation.MovieCommand> mediaStation) {
+        return Behaviors.setup(context -> new UI(context, tempSensor, airCondition, weatherSensor, mediaStation));
     }
 
     private  UI(ActorContext<Void> context,
                 ActorRef<TemperatureSensor.TemperatureCommand> tempSensor,
                 ActorRef<AirCondition.AirConditionCommand> airCondition,
-                ActorRef<WeatherSensor.WeatherCommand> weatherSensor) {
+                ActorRef<WeatherSensor.WeatherCommand> weatherSensor,
+                ActorRef<MediaStation.MovieCommand> mediaStation) {
 
         super(context);
         // TODO: implement actor and behavior as needed
@@ -37,6 +41,7 @@ public class UI extends AbstractBehavior<Void> {
         this.airCondition = airCondition;
         this.tempSensor = tempSensor;
         this.weatherSensor = weatherSensor;
+        this.mediaStation = mediaStation;
         new Thread(() -> { this.runCommandLine(); }).start();
 
         getContext().getLog().info("UI started");
@@ -70,6 +75,9 @@ public class UI extends AbstractBehavior<Void> {
             }
             if(command[0].equals("w")) {
                 this.weatherSensor.tell(new WeatherSensor.ReadWeather(Optional.of(String.valueOf(command[1]))));
+            }
+            if(command[0].equals("m")) {
+                this.mediaStation.tell(new MediaStation.ReadMediaStationStatus(Optional.of(Boolean.valueOf(command[1]))));
             }
             // TODO: process Input
         }
