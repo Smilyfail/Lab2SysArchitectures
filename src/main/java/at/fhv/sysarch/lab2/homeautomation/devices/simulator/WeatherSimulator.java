4a36c681424db41f28551271351d0de870ce72a2
Smilyfail <dario.birbamer@gmail.com>
2022-05-12 21:13:20 +0200

    Fixed some timers

--- src/main/java/at/fhv/sysarch/lab2/homeautomation/devices/simulator/WeatherSimulator.java
+++ src/main/java/at/fhv/sysarch/lab2/homeautomation/devices/simulator/WeatherSimulator.java
@@ -34,7 +34,7 @@ public class WeatherSimulator extends AbstractBehavior<WeatherSimulator.WeatherS
         this.deviceId = deviceId;
 
         getContext().getLog().info("WeatherSimulator started");
-        scheduler.startTimerAtFixedRate(new WeatherUpdate(), Duration.ofSeconds(90));
+        scheduler.startTimerAtFixedRate(new WeatherUpdate(), Duration.ofSeconds(60));
     }
 
     @Override
